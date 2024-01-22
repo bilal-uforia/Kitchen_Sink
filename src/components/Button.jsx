e3ae06ab548832762b9a76dce5df0a73198c18a3
bilal-uforia <156321686+bilal-uforia@users.noreply.github.com>
2024-01-22 12:41:02 +0500

    added text action buttons

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 
 
-const Button = ({ children, customCLass, bgColor, onClickHandler, attributes }) => {
+const Button = ({ children, customClass, bgColor, onClickHandler, attributes }) => {
     return (
         <button className={`py-[4px] px-[14px] text-sm border-[1px] border-[#000000]/15 border-solid rounded-[4px]  
-        ${bgColor || "bg-[#f5f5f5]"} ${customCLass}`}
+        ${bgColor || "bg-[#f5f5f5]"} ${customClass}`}
             onClick={onClickHandler} {...attributes}>
             {children}
         </button>
--- src/components/TabsContent/TextActions.jsx
+++ src/components/TabsContent/TextActions.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
 import { useCanvasContext } from "../../ContextProviders/CanvasContextProvider"
 import { Label } from "./ObjectContent";
+import Button from "../Button";
+
+
+const TextActionButton = ({ name, title, trueValue, falseValue, customClass }) => {
+    const { canvas } = useCanvasContext();
+    const activeObject = canvas.current.getActiveObject();
+
+    const [active, setActive] = useState((trueValue || falseValue) ?
+        activeObject.get(name) == trueValue :
+        activeObject.get(name));
+
+    const onClickHandler = () => {
+
+        if (trueValue || falseValue) {
+            active ? activeObject.set(name, falseValue) : activeObject.set(name, trueValue)
+        }
+        else {
+            activeObject?.set(name, !active);
+        }
+        setActive((prev) => !prev);
+        canvas.current.renderAll();
+    }
+
+    return <Button onClickHandler={onClickHandler} attributes={{ disabled: !activeObject }} customClass={customClass}>
+        {title}
+    </Button>
+}
+
+
 
 const TextActions = () => {
     const { canvas } = useCanvasContext();
@@ -7,7 +37,38 @@ const TextActions = () => {
 
 
     const onChangeHandler = (e) => {
-        activeObject.set(e?.target?.name, e?.target?.value);
+        let value = e?.target?.value;
+
+        switch (e?.target?.type) {
+            case "range":
+                value = Number(e?.target?.value)
+                break;
+            case "checkbox":
+                value = e?.target?.checked;
+                break;
+        }
+
+        activeObject.set(e?.target?.name, value);
+        canvas.current.renderAll();
+    }
+
+    //adding subscript that will increase 2 time size and baseline 0.89
+    const addSubscript = () => {
+        if (activeObject?.subscript?.size == 0.6) {
+            activeObject.set("subscript", { size: 2, baseline: 0.89 });
+            activeObject.setSubscript(0, 6);
+            canvas.current.renderAll();
+        }
+        else {
+            activeObject.set("subscript", { size: 0.6, baseline: 0.11 });
+            activeObject.setSubscript();
+            canvas.current.renderAll();
+        }
+    }
+
+    const addSuperscript = () => {
+        activeObject.set("superscript", { size: 2, baseline: 0.89 });
+        activeObject.setSuperscript(0, 6);
         canvas.current.renderAll();
     }
 
@@ -23,10 +84,10 @@ const TextActions = () => {
 
         <div className="flex gap-2">
             <Label>Font family:</Label>
-            <select name="fontFamily" onChange={onChangeHandler}
+            <select name="fontFamily" onChange={onChangeHandler} defaultValue={activeObject?.fontFamily || "helvetica"}
                 className="border-[1px] border-solid border-[#cccccc] rounded cursor-pointer">
                 <option value="arial">Arial</option>
-                <option value="helvetica" selected="">Helvetica</option>
+                <option value="helvetica">Helvetica</option>
                 <option value="myriad pro">Myriad Pro</option>
                 <option value="delicious">Delicious</option>
                 <option value="verdana">Verdana</option>
@@ -44,6 +105,53 @@ const TextActions = () => {
 
         <div className="flex gap-2">
             <Label>Text Align:</Label>
+            <select name="textAlign" onChange={onChangeHandler} defaultValue={activeObject?.textAlign || "left"}
+                className="border-[1px] border-solid border-[#cccccc] rounded cursor-pointer">
+                <option value="left">Left</option>
+                <option value="center">Center</option>
+                <option value="right">Right</option>
+                <option value="justify">Justify</option>
+                <option value="justify-left">Justify-left</option>
+                <option value="justify-right">Justify-right</option>
+                <option value="justify-center">Justify-center</option>
+            </select>
+        </div>
+
+        <div className="flex gap-2">
+            <Label>Background text color:</Label>
+            <input type="color" name="textBackgroundColor"
+                defaultValue={activeObject ? activeObject?.get("textBackgroundColor") : "#0000"}
+                onChange={onChangeHandler} />
+        </div>
+
+        <div className="flex gap-3">
+            <Label>Font Size:</Label>
+            <input disabled={!activeObject} name="fontSize" className="w-[200px]" type="range"
+                defaultValue={activeObject ? activeObject?.get("fontSize") : 40}
+                step={1} min="0" max="120" onChange={onChangeHandler} />
+        </div>
+
+        <div className="flex gap-3">
+            <Label>Line height:</Label>
+            <input disabled={!activeObject} name="lineHeight" className="w-[200px]" type="range"
+                defaultValue={activeObject ? activeObject?.get("lineHeight") : 1.16}
+                step={0.1} min="0" max="10" onChange={onChangeHandler} />
+        </div>
+
+        <div className="flex gap-3">
+            <Label>Char spacing:</Label>
+            <input disabled={!activeObject} name="charSpacing" className="w-[200px]" type="range"
+                defaultValue={activeObject ? activeObject?.get("charSpacing") : 0}
+                step={10} min="-200" max="800" onChange={onChangeHandler} />
+        </div>
+
+        <div className="flex gap-2 flex-wrap">
+            <TextActionButton title="Bold" name="fontWeight" trueValue="bold" falseValue={"400"} />
+            <TextActionButton title="Italic" name="fontStyle" trueValue="italic" falseValue={"normal"} />
+            <TextActionButton title="Underline" name="underline" customClass="underline" />
+            <TextActionButton title="Linethrough" name="linethrough" customClass="line-through" />
+            <TextActionButton title="Overline" name="overline" customClass="overline" />
+            <Button onClickHandler={addSubscript}>Subscript</Button>
         </div>
 
     </div>
