05086667fa2545d432fc29a5380389d52d6ec540
bilal-uforia <156321686+bilal-uforia@users.noreply.github.com>
2024-01-24 16:38:00 +0500

    fixed button issue

--- src/components/TabsContent/RasterizeCanvas.jsx
+++ src/components/TabsContent/RasterizeCanvas.jsx
@@ -29,13 +29,13 @@ const RasterizeCanvas = () => {
         <div className="flex gap-2 items-baseline">
             <p className='text-md mb-[10px] text-[#333333]'>Rasterize canvas to</p>
             <div className="flex items-center gap-2">
-                <Button customClass="bg-[#5bb75b] text-[#ffffff] font-semibold" onClickHandler={generateImage}
+                <Button customClass="!bg-[#5bb75b] text-[#ffffff] font-semibold" onClickHandler={generateImage}
                     attributes={{ disabled: !canvas }}>Image</Button>
-                <Button customClass="bg-[#5bb75b] text-[#ffffff] font-semibold" onClickHandler={generateImage}
+                <Button customClass="!bg-[#5bb75b] text-[#ffffff] font-semibold" onClickHandler={generateImage}
                     attributes={{ disabled: !canvas }}>Image 3X multiplied</Button>
-                <Button customClass="bg-[#5bb75b] text-[#ffffff] font-semibold" attributes={{ disabled: !canvas }}
+                <Button customClass="!bg-[#5bb75b] text-[#ffffff] font-semibold" attributes={{ disabled: !canvas }}
                     onClickHandler={generateSvg}>SVG</Button>
-                <Button customClass="bg-[#5bb75b] text-[#ffffff] font-semibold"
+                <Button customClass="!bg-[#5bb75b] text-[#ffffff] font-semibold"
                     attributes={{ disabled: !canvas }}
                     onClickHandler={generateJSON}>JSON</Button>
             </div>
